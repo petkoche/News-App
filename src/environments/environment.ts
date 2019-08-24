bbe1d0a1a40c7437584eb3fd9ce5832f56ef9243
petkoche <petkoche90@gmail.com>
2019-08-24 12:55:17 +0300

    clean up and add enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  newsApiUrl: 'https://newsapi.org/v2/everything',
+  apikey: '51bb3060d9d14205916c8d45dd6efd0d'
 };
 
 /*
